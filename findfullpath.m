a5e7c45d765184343ff8d55afb3696bd49a38e8b
formanet420 <t.formanek@outlook.cz>
2021-02-15 00:24:41 +0100

    Update findfullpath.m
    function improvement

--- findfullpath.m
+++ findfullpath.m
@@ -4,8 +4,8 @@ fname = 0;
 files = struct2cell(files);
 for i = 3:(length(files))
     filename = char(files(1, i));
-    filename = filename(1:15);
-    if strcmp(filename, keyword)
+    
+    if contains(filename, keyword) 
         fname = [char(files(2,i)) '\' char(files(1,i))];
     end
 end
